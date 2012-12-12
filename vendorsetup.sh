890ff8c3992067d3d55afa077bfb9f3d13c1392a
Papa-Smurf151 <cbarlan@gmail.com>
2012-12-11 22:07:07 -0500

    intial commit

---
+++ vendorsetup.sh
@@ -0,0 +1,23 @@
+# Xperia 2011
+add_lunch_combo pac_anzu-userdebug
+add_lunch_combo pac_haida-userdebug
+add_lunch_combo pac_mango-userdebug
+add_lunch_combo pac_mint-userdebug
+add_lunch_combo pac_smultron-userdebug
+add_lunch_combo pac_urushi-userdebug
+add_lunch_combo pac_zeus-userdebug
+add_lunch_combo pac_zeusc-userdebug
+# Galaxy S2 Series
+add_lunch_combo pac_hercules-userdebug
+add_lunch_combo pac_i9100-userdebug
+add_lunch_combo pac_i9100g-userdebug
+add_lunch_combo pac_quincyatt-userdebug
+add_lunch_combo pac_skyrocket-userdebug
+add_lunch_combo pac_i927-userdebug
+add_lunch_combo pac_d710-userdebug
+# Galaxy Ace
+add_lunch_combo pac_cooper-userdebug
+# Nexus 7
+add_lunch_combo pac_grouper-userdebug
+# Galaxy S3
+add_lunch_combo pac_d2spr-userdebug
